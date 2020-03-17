d85d61f581091b9e3109862177e71eb69280b31e
Renan Borba <48495838+RenanBorba@users.noreply.github.com>
2020-03-17 10:26:18 -0300

    Update styles.js

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -2,19 +2,19 @@ import styled from 'styled-components/native';
 import { Animated } from 'react-native';
 
 export const Container = styled(Animated.ScrollView)`
-  margin: -20px 30px;
+  margin: -20px 30px
 `;
 
 export const Code = styled.View`
   background: #FFF;
   padding: 10px;
-  align-self: center;
+  align-self: center
 `
 
 export const Nav = styled.View`
   margin-top: 20px;
   border-top-width: 1px;
-  border-top-color: rgba(255, 255, 255, 0.4);
+  border-top-color: rgba(255, 255, 255, 0.4)
 `
 
 export const NavItem = styled.View`
@@ -22,7 +22,7 @@ export const NavItem = styled.View`
   align-items: center;
   padding: 8px 0;
   border-bottom-width: 1px;
-  border-bottom-color: rgba(255, 255, 255, 0.4);
+  border-bottom-color: rgba(255, 255, 255, 0.4)
 `
 
 export const NavText = styled.Text`
@@ -43,5 +43,5 @@ export const SignOutButton = styled.TouchableOpacity`
 
 export const SignOutButtonText = styled.Text`
   color: #FFF;
-  font-weight: bold;
+  font-weight: bold
 `
